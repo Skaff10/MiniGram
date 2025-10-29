5c0e519da216332182031cdecf68d1b173457df9
Skaff10 <str.skaff@gmail.com>
2025-10-30 04:09:21 +0600

    post done

---
+++ backend/controller/feedController.js
@@ -0,0 +1,41 @@
+const Post = require("../models/postModel");
+const Comment = require("../models/commentModel");
+const asyncHandler = require("express-async-handler");
+
+// get all post for a single post to show in my profile section
+const getProfilePost = asyncHandler(async (req, res) => {
+  const posts = await Post.find({ user: req.user._id })
+    .sort({ createdAt: -1 })
+    .populate("user", "user_name profilePic");
+
+  for (let post of posts) {
+    const commment = await Comment.find({ post: post._id })
+      .sort({ createdAt: -1 })
+      .populate("user", "user_name profilePic")
+      .lean();
+    post.commments = commment.reverse();
+    postLikeCount = post.likes.length;
+  }
+
+  res.status(200).json(posts);
+});
+
+// Get all posts for feed
+const getFeedPosts = asyncHandler(async (req, res) => {
+  const posts = await Post.find()
+    .sort({ createdAt: -1 })
+    .populate("user", "user_name profilePic");
+  for (let post of posts) {
+    const commment = await Comment.find({ post: post._id })
+      .sort({ createdAt: -1 })
+      .populate("user", "user_name profilePic")
+      .lean();
+    post.commments = commment.reverse();
+    postLikeCount = post.likes.length;
+  }
+});
+
+module.exports = {
+  getFeedPosts,
+  getProfilePost,
+};
---
+++ backend/controller/postController.js
@@ -0,0 +1,69 @@
+const Post = require("../models/postModel");
+const asyncHandler = require("express-async-handler");
+const createPost = asyncHandler(async (req, res) => {
+  const { text, image } = req.body;
+  if (!text && !image) {
+    res.status(400);
+    throw new Error("There is nothing to Post");
+  }
+
+  const post = await Post.create({
+    user: req.user._id,
+    text: text || "",
+    image: image || null,
+  });
+
+  await post.populate("user", "user_name profilePic");
+  res.status(201).json(post);
+});
+
+const updatePost = asyncHandler(async (req, res) => {
+  const post = await Post.findById(req.params.id);
+  if (!post) {
+    res.status(404);
+    throw new Error("Post Not Found!!");
+  }
+
+  if (req.user) {
+    res.status(401);
+    throw new Error("User not found");
+  }
+
+  if (post.user.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error("User not authorized");
+  }
+
+  const updatedPost = await Post.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+  res.status(200).json(updatedPost);
+});
+
+const deletePost = asyncHandler(async (req, res) => {
+  const post = await Post.findById(req.params.id);
+  if (!post) {
+    res.status(404);
+    throw new Error("Post Not Found!!");
+  }
+
+  if (req.user) {
+    res.status(401);
+    throw new Error("User not found");
+  }
+
+  if (post.user.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error("User not authorized");
+  }
+
+  await Post.findByIdAndDelete(req.params.id);
+  res.status(200).json({ id: req.params.id });
+});
+
+module.exports = {
+  createPost,
+  getAllPost,
+  updatePost,
+  deletePost,
+};
